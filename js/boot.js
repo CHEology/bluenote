22bc461d715888780c4cf15f4286b012e9c2d420
CHEology <zl5012@nyu.edu>
2026-08-26 20:17:21 -0400

    Simplify article footers

--- js/boot.js
+++ js/boot.js
@@ -17,6 +17,43 @@ Fluid.boot.refresh = function() {
   Fluid.events.refresh();
 };
 
+Fluid.boot.simplifyPostFooter = function() {
+  var post = document.querySelector('article.post-content');
+  if (!post) return;
+
+  var tags = post.querySelector('.post-metas.my-3');
+  var license = post.querySelector('.license-box');
+  var footer = license ? license.parentElement : (tags ? tags.parentElement : null);
+
+  if (tags) tags.remove();
+
+  if (license) {
+    var titleSource = license.querySelector('.license-title > div:first-child');
+    var dateSource = license.querySelector('.license-meta-date > div:last-child');
+    var publishedTime = document.querySelector('.banner-text time[datetime]');
+    var title = document.createElement('div');
+    var date = document.createElement('time');
+
+    title.className = 'post-end-title';
+    title.textContent = titleSource ? titleSource.textContent.trim() : '';
+    date.className = 'post-end-date';
+    date.textContent = dateSource ? dateSource.textContent.trim() : '';
+
+    if (publishedTime) {
+      date.setAttribute('datetime', publishedTime.getAttribute('datetime'));
+    }
+
+    license.replaceChildren(title, date);
+    license.className = 'post-end-card my-3';
+  }
+
+  if (footer) {
+    var divider = footer.previousElementSibling;
+    if (divider && divider.tagName === 'HR') divider.remove();
+  }
+};
+
 document.addEventListener('DOMContentLoaded', function() {
   Fluid.boot.registerEvents();
+  Fluid.boot.simplifyPostFooter();
 });
